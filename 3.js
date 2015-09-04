7ad8304930687669dff9b2102be9e9e5c7b5d4f2
Yatish Mehta <yatishmehta27@gmail.com>
2015-09-04 11:34:36 -0400

    Uncomments functions calls

--- 3.js
+++ 3.js
@@ -9,17 +9,17 @@ function fullName(firstName, lastName) {
 
 var sum = function(a, b) { return (a + b); };
 
-//(function () {
-//	console.log("I am inside");    
-//}());
+(function () {
+	console.log("I am inside");    
+}());
 
-//name();
-//console.log(fullName("Yatish", "Mehta"));
-//console.log(sum(2, 8));
+name();
+console.log(fullName("Yatish", "Mehta"));
+console.log(sum(2, 8));
 
-//console.log(sum.call(this, 2, 8));
+console.log(sum.call(this, 2, 8));
 
-//Check for default value inside the function 
+// Check for default value inside the function 
 // in JS function is first class citizen
 
 function foo(handler) {
